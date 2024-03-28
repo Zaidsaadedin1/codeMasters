1016aa35c59803d0efbd078183a3b31ae365bcdf
zaid <zaidsaadedin10@gmail.com>
2024-03-28 22:03:15 +0300

    fex merge issue

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,13 +1,15 @@
 "use client";
 import React, { useState, useEffect } from "react";
 import "./Login.css";
-<<<<<<< HEAD
 import { fetchData } from "@/Apis/Apis";
 import { UserLoginDto } from "@/Interfaces/User/UserLoginDto";
 import { Button } from "../ui/button";
 import { useRouter } from "next/navigation";
+import { useAuthintications } from "@/Contexts/AuthProvider";
 
 function Login() {
+  const { setToken } = useAuthintications();
+
   const [userLogins, setUserLogins] = useState<UserLoginDto>({
     email: "",
     password: "",
@@ -16,13 +18,11 @@ function Login() {
 
   const submitLogins = async () => {
     const response = await fetchData.login(userLogins);
-
-    if (response.status === 200) {
-      console.log("Login successful");
-      console.log(response);
-      router.push("/AdminPanel");
+    if (response.status === 200 && response.data.token) {
+      setToken(response.data.token);
+      router.push("/admin-panel");
     } else {
-      console.error("Login failed");
+      router.push("/login");
     }
   };
 
@@ -30,6 +30,58 @@ function Login() {
     <>
       <div>
         <section>
+          <span></span> <span></span> <span></span> <span></span> <span></span>{" "}
+          <span></span> <span></span> <span></span> <span></span> <span></span>{" "}
+          <span></span> <span></span> <span></span> <span></span> <span></span>{" "}
+          <span></span> <span></span> <span></span> <span></span> <span></span>{" "}
+          <span></span> <span></span> <span></span> <span></span> <span></span>{" "}
+          <span></span> <span></span> <span></span> <span></span> <span></span>{" "}
+          <span></span> <span></span> <span></span> <span></span> <span></span>{" "}
+          <span></span> <span></span> <span></span> <span></span> <span></span>{" "}
+          <span></span> <span></span> <span></span> <span></span> <span></span>{" "}
+          <span></span> <span></span> <span></span> <span></span> <span></span>{" "}
+          <span></span> <span></span> <span></span> <span></span> <span></span>{" "}
+          <span></span> <span></span> <span></span> <span></span> <span></span>{" "}
+          <span></span> <span></span> <span></span> <span></span> <span></span>{" "}
+          <span></span> <span></span> <span></span> <span></span> <span></span>{" "}
+          <span></span> <span></span> <span></span> <span></span> <span></span>{" "}
+          <span></span> <span></span> <span></span> <span></span> <span></span>{" "}
+          <span></span> <span></span> <span></span> <span></span> <span></span>{" "}
+          <span></span> <span></span> <span></span> <span></span> <span></span>{" "}
+          <span></span> <span></span> <span></span> <span></span> <span></span>{" "}
+          <span></span> <span></span> <span></span> <span></span> <span></span>{" "}
+          <span></span> <span></span> <span></span> <span></span> <span></span>{" "}
+          <span></span> <span></span> <span></span> <span></span> <span></span>{" "}
+          <span></span> <span></span> <span></span> <span></span> <span></span>{" "}
+          <span></span> <span></span> <span></span> <span></span> <span></span>{" "}
+          <span></span> <span></span> <span></span> <span></span> <span></span>{" "}
+          <span></span> <span></span> <span></span> <span></span> <span></span>{" "}
+          <span></span> <span></span> <span></span> <span></span> <span></span>{" "}
+          <span></span> <span></span> <span></span> <span></span> <span></span>{" "}
+          <span></span> <span></span> <span></span> <span></span> <span></span>{" "}
+          <span></span> <span></span> <span></span> <span></span> <span></span>{" "}
+          <span></span> <span></span> <span></span> <span></span> <span></span>{" "}
+          <span></span> <span></span> <span></span> <span></span> <span></span>{" "}
+          <span></span> <span></span> <span></span> <span></span> <span></span>{" "}
+          <span></span> <span></span> <span></span> <span></span> <span></span>{" "}
+          <span></span> <span></span> <span></span> <span></span> <span></span>{" "}
+          <span></span> <span></span> <span></span> <span></span> <span></span>{" "}
+          <span></span> <span></span> <span></span> <span></span> <span></span>{" "}
+          <span></span> <span></span> <span></span> <span></span> <span></span>{" "}
+          <span></span> <span></span> <span></span> <span></span> <span></span>{" "}
+          <span></span> <span></span> <span></span> <span></span> <span></span>{" "}
+          <span></span> <span></span> <span></span> <span></span> <span></span>{" "}
+          <span></span> <span></span> <span></span> <span></span> <span></span>{" "}
+          <span></span> <span></span> <span></span> <span></span> <span></span>{" "}
+          <span></span> <span></span> <span></span> <span></span> <span></span>{" "}
+          <span></span> <span></span> <span></span> <span></span> <span></span>{" "}
+          <span></span> <span></span> <span></span> <span></span> <span></span>{" "}
+          <span></span> <span></span> <span></span> <span></span> <span></span>{" "}
+          <span></span> <span></span> <span></span> <span></span> <span></span>{" "}
+          <span></span> <span></span> <span></span> <span></span> <span></span>{" "}
+          <span></span> <span></span> <span></span> <span></span> <span></span>{" "}
+          <span></span> <span></span> <span></span> <span></span> <span></span>{" "}
+          <span></span> <span></span> <span></span> <span></span> <span></span>
           <div className="signin">
             <div className="content">
               <h2>Sign In</h2>
@@ -62,69 +114,6 @@ function Login() {
                   </Button>
                 </div>
               </div>
-=======
-import { fetchData } from '@/Apis/Apis';
-import { UserLoginDto } from '@/Interfaces/User/UserLoginDto';
-import { Button } from '../ui/button';
-import { useRouter } from 'next/navigation';
-import { useAuthintications } from '@/Contexts/AuthProvider';
-
-function Login() {
-    const { setToken } = useAuthintications();
-
-    const [userLogins, setUserLogins] = useState<UserLoginDto>({
-        email: "",
-        password: "",
-    });
-    const router = useRouter();
-
-    const submitLogins = async () => {
-        const response = await fetchData.login(userLogins);
-        if (response.status === 200 && response.data.token) {
-            setToken(response.data.token);
-            router.push("/admin-panel");
-        } else {
-            router.push("/login");
-        }
-    };
-
-    return (
-        <>
-            <div>
-                <section>
-                    <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span>
-                    <div className="signin">
-                        <div className="content">
-                            <h2>Sign In</h2>
-                            <div className="form">
-                                <div className="inputBox">
-                                    <input
-                                        name="email"
-                                        onChange={(e) => setUserLogins({ ...userLogins, email: e.target.value })}
-                                        type="email"
-                                        required
-                                    />
-                                    <i>Email</i>
-                                </div>
-                                <div className="inputBox">
-                                    <input
-                                        onChange={(e) => setUserLogins({ ...userLogins, password: e.target.value })}
-                                        name="password"
-                                        type="password"
-                                        required
-                                    />
-                                    <i>Password</i>
-                                </div>
-                                <div className="inputBox">
-                                    <Button onClick={submitLogins} type="submit" value="Login">
-                                        Login
-                                    </Button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </section>
->>>>>>> 76b20dc00f39b12bc8940e614cefa3be4bd73d1e
             </div>
           </div>
         </section>
