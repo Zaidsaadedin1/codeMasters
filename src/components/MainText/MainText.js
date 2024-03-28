7dbaa402345a40f15f09aab3c918232852c22172
zaid <zaidsaadedin10@gmail.com>
2024-03-29 00:27:39 +0300

    fexed main text

--- src/components/MainText/MainText.js
+++ src/components/MainText/MainText.js
@@ -3,7 +3,7 @@ import Animations from "./Animations";
 
 function MainText() {
   return (
-    <div className="bg-white flex flex-row  flex-wrap justify-end pt-2">
+    <div className="bg-white flex flex-row  flex-wrap justify-end p-5">
       <section className="animation "></section>
       <section className="">
         <motion.div
@@ -16,8 +16,10 @@ function MainText() {
             احنا موجودين عشان نساعدك تتعدى التحديات في عالم البرمجة
           </h3>
           <h4 className="text-2xl leading-relaxed font-mono">
-            عم تدرس منقدر انساعدك ونقدملك خدمات مواقع الويب أو تطبيقات الهاتف او
-            دروس خصوصية وخدمات مختلفة إحنا هون عشان ندعمكم!
+            عم تدرس منقدر انساعدك ونقدملك خدمات مواقع الويب أو تطبيقات الهاتف{" "}
+            <br />
+            او دروس خصوصية وبحوث علمية وخدمات مختلفة إحنا هون عشان ندعمكم
+            <br />
             <span className="text-2xl font-mono">
               قدم طلب لنساعدك في مشاريعك او في اعطائك دروس خصوصية
             </span>
