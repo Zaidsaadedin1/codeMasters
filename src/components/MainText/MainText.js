2337972cbc6dd3343b02f9229732e2f5408b48bf
zaid <zaidsaadedin10@gmail.com>
2024-03-29 04:33:35 +0300

    change the main text content

--- src/components/MainText/MainText.js
+++ src/components/MainText/MainText.js
@@ -13,15 +13,15 @@ function MainText() {
           className="flex flex-col text-black text-right"
         >
           <h3 className="font-mono text-3xl font-bold tracking-wide mb-5 flex flex-row justify-end items-center">
-            احنا موجودين عشان نساعدك تتعدى التحديات في عالم البرمجة
+            احنا موجودين عشان نساعدك تتعدى التحديات في مسيرتك الجامعية
           </h3>
           <h4 className="text-2xl leading-relaxed font-mono">
-            عم تدرس منقدر انساعدك ونقدملك خدمات مواقع الويب أو تطبيقات الهاتف{" "}
+            عم تدرس منقدر انساعدك ونقدملك مشاريع تخرج واعداد الاوراق النقاشية
             <br />
             او دروس خصوصية وبحوث علمية وخدمات مختلفة إحنا هون عشان ندعمكم
             <br />
             <span className="text-2xl font-mono">
-              قدم طلب لنساعدك في مشاريعك او في اعطائك دروس خصوصية
+              قدم طلب لنساعدك في مشوارك
             </span>
           </h4>
         </motion.div>
