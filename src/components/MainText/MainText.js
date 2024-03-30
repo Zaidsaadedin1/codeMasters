c12d3cdc0bc0437e52b871d4c8fa543b24a1ebbc
zaid <zaidsaadedin10@gmail.com>
2024-03-30 23:16:40 +0300

    change the main text size and colors

--- src/components/MainText/MainText.js
+++ src/components/MainText/MainText.js
@@ -8,22 +8,22 @@ function MainText() {
       <section className="">
         <motion.div
           initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
+          animate={{ opacity: 2 }}
           transition={{ duration: 1 }}
           className="flex flex-col text-black text-right"
         >
-          <h3 className="font-mono text-3xl font-bold tracking-wide mb-5 flex flex-row justify-end items-center">
+          <h1 className="font-mono text-3xl font-bold tracking-wide mb-5 flex flex-row justify-end items-center">
             احنا موجودين عشان نساعدك تتعدى التحديات في مسيرتك الجامعية
-          </h3>
-          <h4 className="text-2xl leading-relaxed font-mono">
+          </h1>
+          <h2 className="text-2xl leading-relaxed font-mono">
             عم تدرس منقدر انساعدك ونقدملك مشاريع تخرج واعداد الاوراق النقاشية
             <br />
             او دروس خصوصية وبحوث علمية وخدمات مختلفة إحنا هون عشان ندعمكم
             <br />
-            <span className="text-2xl font-mono">
+            <span className="text-2xl font-mono text-red-400">
               قدم طلب لنساعدك في مشوارك
             </span>
-          </h4>
+          </h2>
         </motion.div>
       </section>
     </div>
